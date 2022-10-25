5db151ccf91341d1c644eff01eaf1688315469f5
MattiaLongo03 <mattilongo2003@gmail.com>
2022-10-25 16:26:00 +0200

    commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const emailList =['mario@gmail.com','lucagmail.com','giorgio@gmail.com'];
+const userEmail = prompt('Inserisci la tua email');
+
+if(emailList.includes(userEmail)){
+    console.log('Email confermata');
+    nUser = Math.floor(Math.random() * 6) + 1;
+    nCPU = Math.floor(Math.random() * 6) + 1;
+    console.log('Il tuo numero è:' + nUser);
+    console.log('Il mio numero è:' + nCPU);
+    if(nUser<nCPU){
+        console.log('Hai perso!');
+    }else
+        console.log('Hai vinto!');
+    if(nUser == nCPU){
+        console.log('Pareggio');
+    }
+}else
+    console.log('Email rifiutata');
